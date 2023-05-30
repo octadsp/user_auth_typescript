6db409ab5f2338c12de3d7f2e67fa6d14781a5db
tole <octaadsp.camp@gmail.com>
2023-05-30 14:23:49 +0700

    Helpers & Controllers

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import crypto from 'crypto';
+
+const SECRET = 'OCTAADSP-REST-API';
+
+// crypto.randomBytes(128) untuk menghasilkan 128 bytes secara acak kemudian diubah menjadi format string dengan metode toString dengan enkoding base64
+export const random = () => crypto.randomBytes(128).toString('base64');
+
+// Fungsi ini menerima 2 parameter (salt , password)
+// Crypto.createHmac('sha256') = membuat instance objek HMAC (Hash-based Message Authentication Code) dengan algoritma hash SHA-256.
+// Nilai salt & password digabungkan dengan method join menggunakan ('/') sebagai tanda pemisahnya dan digunakan sebagai kunci untuk HMAC.
+// Method .update(SECRET) digunakan untuk mengupdate HMAC dengan nilai SECRET
+// Method .digest() digunakan untuk menghasilkan nilai hash dari HMAC
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest();
+};
